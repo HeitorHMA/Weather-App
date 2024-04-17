f369f7e69642abc4681975adf1300e39d2dab7ee
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-04-17 11:23:05 -0300

    Fetch name and time

--- src/fetchLocationName.js
+++ src/fetchLocationName.js
@@ -8,8 +8,18 @@ export default function fetchLocationName(){
         return response.json();
     })
     .then(function(response) {
-        const userLocationTemp = document.querySelector("#userLocationTemp");
-        userLocationTemp.innerHTML = `${response.location.name},${response.location.country}`
-        console.log(response)
-      });
-    }
+        const userLocationTemp = document.querySelector("#userLocationPlace");
+        userLocationTemp.innerHTML = `${response.location.name}, ${response.location.country}`;
+      })
+
+    fetch (`https://api.weatherapi.com/v1/current.json?key=58f97e0302ee40a28e5192754241504&q=${userInput}`,{mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response) {
+      const userLocationTime = document.querySelector("#userLocationTime");
+      userLocationTime.innerHTML = `${response.location.localtime}`;
+      console.log(response)
+    })
+  
+    };
